d287b2447a6a87570dbc7433a21da0fa249080e0
futsu-git <futsu.develop@gmail.com>
2022-01-05 10:12:22 +0900

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ hi: "there" });
+	res.send({ bye: "buddy" });
 });
 
 // expressがnodeにポート5000番を監視させる
